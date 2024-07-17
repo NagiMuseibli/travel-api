c40c9e718b18c22de4a1cac336bd89b6d4a600ac
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-18 01:12:11 +0400

    Creating Tour model and migration

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -6,6 +6,7 @@ use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
@@ -20,6 +21,11 @@ class Travel extends Model
         'description',
         'number_of_days'
     ];
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
 
     public function sluggable(): array
     {
