7daa3b0986ab547753195c7c9047aa70a53e26eb
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-18 01:22:16 +0400

    IDs to UUIDs

--- database/migrations/2024_07_17_192712_create_role_user_table.php
+++ database/migrations/2024_07_17_192712_create_role_user_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignUuid('role_id')->constrained();
+            $table->foreignUuid('user_id')->constrained();
         });
     }
 
