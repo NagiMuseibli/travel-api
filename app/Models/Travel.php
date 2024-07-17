2444000e253c84cc9e9de337cbfc6d6c235b0725
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-17 23:48:05 +0400

    Creating TRavel model and migration

---
+++ app/Models/Travel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'travels';
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days'
+    ];
+
+}
