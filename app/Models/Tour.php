4b2dd451684c0007087273e0119cf43bab836b64
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-18 01:15:59 +0400

    Price Accessor and Mutator

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,13 @@ class Tour extends Model
         'ending_date',
         'price'
     ];
+
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn($value) => $value * 100
+        );
+
+    }
 }
