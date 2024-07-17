8cef4ea28f00c47ad9837a1e6d2cf7920d36d503
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-18 01:05:15 +0400

    number_of_nights Accessor

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -28,5 +29,13 @@ class Travel extends Model
             ]
         ];
     }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+
 
 }
