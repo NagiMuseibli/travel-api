83b587980e7590ce0612a64cf9d13b85df0dae02
NagiMuseibli <nagimuseibli02@gmail.com>
2024-07-18 10:33:56 +0400

    Creating TravelResource and return clear data

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,7 @@ class TravelController extends Controller
 {
     public function index()
     {
-        return Travel::where('is_public', true)->get();
+        $travels = Travel::where('is_public', true)->get();
+        return TravelResource::collection($travels);
     }
 }
